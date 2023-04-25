9514f142c0c2460c024df4308d6a66af573506aa
Demi Alabi <demialabi98@gmail.com>
2023-04-25 11:59:20 -0400

    workbook2excersises

---
+++ exercises/Mathscripts/net_worth.js
@@ -0,0 +1,11 @@
+var assets = 100000;
+var debts = 20000;
+//calculation
+var netWorth = assets - debts;
+var message = "netWorth is "
+console.log(typeof(message));
+
+//output
+console.log( message + netWorth);
+
+
---
+++ exercises/program.js
@@ -0,0 +1,22 @@
+var firstName;      
+firstName = "Demi";
+lastName = "Alabi";
+console.log(" Hello Friend " + firstName + " " + lastName); 
+
+// sample inputs
+var foodCost = 79.25;
+var tax = 6.54;
+var tip = 12.00;
+
+// calculations
+var totalDue = foodCost + tax + tip;
+
+// output
+console.log("The total due is " + totalDue);
+
+
+
+
+
+
+
