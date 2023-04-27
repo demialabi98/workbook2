b9b71802b6808f7af8043def86c88bae13addcc2
Demi Alabi <demialabi98@gmail.com>
2023-04-27 11:24:11 -0400

    new info

---
+++ demos/function.js
@@ -0,0 +1,6 @@
+function letThemKnow(name){
+    const message = "you need some milk!" + " " + name
+    console.log(message);
+}
+
+letThemKnow("Jake");
--- demos/pay -rules.js
+++ demos/pay -rules.js
@@ -1,14 +1,21 @@
-var payRate
-var hoursWorked
-var overRate
-payRate = 25.00
-hoursWorked = 40
+"use strict";
+var payRate = 25.0;
+var hoursWorked = 46;
+var overRate;
+var overTime;
+var overPay;
+var totalGrossPay;
 
-
-if (hoursWorked == 40 ){
- grossPay = hoursWorked * payRate
-console.log(grossPay) }
-else if {
-overRate = payRate * 1.5
-console.log(overRate)
+if (hoursWorked == 40) {
+  var grossPay = hoursWorked * payRate;
+  console.log(grossPay);
+} else if (hoursWorked > 40) {
+  var overRate = payRate * 1.5;
+  console.log(overRate);
+} else {
+ var overTime = hoursWorked - 40;
+ var overPay = overTime * overRate;
+ var totalGrossPay = grossPay + overPay;
+  
+ 
 }
